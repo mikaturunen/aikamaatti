924b8eb8a7aaf2bc1897e0a5002a32efce649ff9
Mika Turunen <mika.turunen@ymail.com>
2014-12-29 15:05:42 +0200

    moving towards reservation instead of gallery

---
+++ 2.client/reservations/reservations-controller.ts
@@ -0,0 +1,40 @@
+
+module ReservationsController {
+  // The actual controller function that takes in what dependencies were injected.
+  // parameter list for the function always needs to match the injected object list in the below angular module setup
+  // phase
+  var controller = ($scope: ReservationsScope, socket: any, log: any) => {
+    log.debug("Started: " + JSON.stringify(ReservationsController.meta));
+    defineScopeFunctions($scope);
+  };
+
+  // One place to define the functionality of the objects in the scope
+  var defineScopeFunctions = ($scope: ReservationsScope) => {  
+    
+  };
+
+  /**
+   * ReservationsScope object for $scope usage.
+   */
+  export interface ReservationsScope extends ng.IScope {
+    
+  };
+
+  /**
+   * Module details for Angular to consume.
+   */ 
+  export var meta: MetaModule = {
+    moduleName: "reservations",
+    controllerName: "ReservationsController",
+    controllerFunction: controller
+  }
+}
+
+// Setup angular module, dependencies and behavior
+angular
+  .module(ReservationsController.meta.moduleName)
+  .controller(ReservationsController.meta.controllerName, 
+    [
+      "$scope", "socket", "log",
+      ReservationsController.meta.controllerFunction 
+    ]);
