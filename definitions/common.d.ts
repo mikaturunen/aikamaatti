d39861bff7a3c3817d02cefa55c34baecd7c9492
Mika Turunen <mika.turunen@ymail.com>
2014-12-31 13:27:24 +0200

    Creating additional interface definitions to maintain control and type checks when the controllers tell their own states to the ui-router

---
+++ definitions/common.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Files referencing the common d.ts files that are shared on both client and server sides.
+ * Collecting all of them into a single file as I'm lazy like that...
+ */
+
+ ///<reference path="DefinitelyTyped/node/node.d.ts" />
+
