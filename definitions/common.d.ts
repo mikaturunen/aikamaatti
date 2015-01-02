76ece593e5bb2ab4006d8d78c692c001b21fb6ff
Mika Turunen <mika.turunen@ymail.com>
2015-01-02 10:13:47 +0200

    Added the first draft of the database models we are going to be using

--- definitions/common.d.ts
+++ definitions/common.d.ts
@@ -3,5 +3,6 @@
  * Collecting all of them into a single file as I'm lazy like that...
  */
 
- ///<reference path="DefinitelyTyped/node/node.d.ts" />
+///<reference path="DefinitelyTyped/node/node.d.ts" />
+///<reference path="Common/database-models.d.ts" />
 
